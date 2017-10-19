0ae3c708d16157bdbcd144280d2d6074ccf13917
seanpierce <sumler.sean@gmail.com>
2017-10-18 19:45:12 -0700

    toggleConfirmation when check box is clicked

--- src/GuestList.js
+++ src/GuestList.js
@@ -5,12 +5,18 @@ import Guest from './Guest'
 const GuestList = props =>
   <ul>
     {props.guests.map((guest, index) =>
-      <Guest key={index} name={guest.name} isConfirmed={guest.isConfirmed} />
+      <Guest
+        key={index}
+        name={guest.name}
+        isConfirmed={guest.isConfirmed}
+        handleConfirmation={() => props.toggelConfirmationAt(index)}
+      />
     )}
   </ul>;
 
 GuestList.propTypes = {
-  guests: PropTypes.array.isRequired
+  guests: PropTypes.array.isRequired,
+  toggelConfirmationAt: PropTypes.func.isRequired
 }
 
 export default GuestList;
